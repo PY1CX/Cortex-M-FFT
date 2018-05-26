2115f13338e9699606e00725fb4d5158f0a93305
Felipe Navarro <felnavarro2@gmail.com>
2018-05-26 18:36:18 -0300

    32, 512, 1024 code OK

---
+++ Cortex-M7/RTE/RTE_Components.h
@@ -0,0 +1,17 @@
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: testm7
+ * RTE configuration: testm7.rteconfig
+*/
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+/*
+ * Define the Device Header File:
+*/
+#define CMSIS_device_header "stm32h7xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
