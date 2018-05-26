11f9c89e11f9591dc820a2966d9cbb00114f2f6c
Felipe Navarro <felnavarro2@gmail.com>
2018-05-26 01:18:24 -0300

    Only 1024 yet

---
+++ Cortex-M3/RTE/RTE_Components.h
@@ -0,0 +1,17 @@
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: aa
+ * RTE configuration: aa.rteconfig
+*/
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+/*
+ * Define the Device Header File:
+*/
+#define CMSIS_device_header "stm32f10x.h"
+
+
+#endif /* RTE_COMPONENTS_H */
