88412d08eafd03935f54e9765781c415decb5623
Felipe Navarro <felnavarro2@gmail.com>
2018-05-26 01:17:09 -0300

    32, 512 and 1024 working on Cortex-M0
    Added final files

---
+++ Cortex-M0/RTE/RTE_Components.h
@@ -0,0 +1,17 @@
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: M0-Mock
+ * RTE configuration: M0-Mock.rteconfig
+*/
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+/*
+ * Define the Device Header File:
+*/
+#define CMSIS_device_header "stm32f0xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
